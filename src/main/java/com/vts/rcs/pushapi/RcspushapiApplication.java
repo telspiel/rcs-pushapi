f54f740e617d61c050fcb8e5e02e8153227e8dbf
Harshtelexe <Harsh.sharma@telspiel.com>
2026-03-06 10:57:02 +0530

    updated code with jar

--- src/main/java/com/vts/rcs/pushapi/RcspushapiApplication.java
+++ src/main/java/com/vts/rcs/pushapi/RcspushapiApplication.java
@@ -1,33 +1,13 @@
-//package com.vts.rcs.pushapi;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication(scanBasePackages = "com.vts.rcs")
-//public class RcspushapiApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(RcspushapiApplication.class, args);
-//	}
-//
-//}
-
 package com.vts.rcs.pushapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.vts.rcs")
-public class RcspushapiApplication extends SpringBootServletInitializer {
+public class RcspushapiApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(RcspushapiApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RcspushapiApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(RcspushapiApplication.class, args);
-    }
 }
